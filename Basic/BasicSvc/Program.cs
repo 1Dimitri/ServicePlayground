11fca3d1c6ce842d831bd33251ad0a51b98efb46
1Dimitri <git.dimitri@janczak.net>
2019-08-18 14:19:26 +0200

    simple basic service doing nothing

---
+++ Basic/BasicSvc/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace BasicSvc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var rc = HostFactory.Run(x =>                                  
+            {
+                x.Service<Service>(s =>                                  
+                {
+                    s.ConstructUsing(name => new Service());                
+                    s.WhenStarted(tc => tc.StartEntryPoint());                         
+                    s.WhenStopped(tc => tc.StopEntryPoint());                          
+                });
+                x.RunAsLocalSystem();                                      
+
+                x.SetDescription("Sample Topshelf Basic Service");            
+                x.SetDisplayName("Basic Service");                                 
+                x.SetServiceName("BasicService");                          
+            });                                                             
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
