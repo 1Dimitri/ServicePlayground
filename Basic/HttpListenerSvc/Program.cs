c39b25935a10647d52d2eafacb66a955ccb4adb9
1Dimitri <git.dimitri@janczak.net>
2019-08-18 15:32:32 +0200

    Http Listener within Service example

---
+++ Basic/HttpListenerSvc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace HttpListenerSvc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var rc = HostFactory.Run(x =>
+            {
+                x.Service<HttpListenerService>(s =>
+                {
+                    s.ConstructUsing(name => new HttpListenerService());
+                    s.WhenStarted(tc => tc.StartEntryPoint());
+                    s.WhenStopped(tc => tc.StopEntryPoint());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Sample Topshelf Http Listener Service");
+                x.SetDisplayName("Basic HttpListener Service");
+                x.SetServiceName("BasicHttpListenerService");
+            });
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            Environment.ExitCode = exitCode;
+
+        }
+    }
+}
---
+++ Basic/HttpListenerSvc/Service.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace HttpListenerSvc
+{
+    class HttpListenerService
+    {
+        private static HttpListener _listener;
+
+        public void StartEntryPoint()
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://localhost/httplistenersvc/");
+            _listener.Start();
+            _listener.BeginGetContext(OnClientConnectionContext, null);
+
+        }
+
+        private static void OnClientConnectionContext(IAsyncResult ar)
+        {
+            var ctx = _listener.EndGetContext(ar);
+            _listener.BeginGetContext(OnClientConnectionContext, null);
+
+            // Console.WriteLine(DateTime.UtcNow.ToString("HH:mm:ss.fff") + " Handling request");
+
+            var buf = Encoding.ASCII.GetBytes($"Hello dear client at {DateTime.UtcNow.ToString()}");
+            ctx.Response.ContentType = "text/plain";
+
+            // simulate work
+            // Thread.Sleep(10000);
+
+            ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+            ctx.Response.OutputStream.Close();
+
+
+            // Console.WriteLine(DateTime.UtcNow.ToString("HH:mm:ss.fff") + " completed");
+        }
+        public void StopEntryPoint()
+        {
+            // Console.WriteLine("Stopping Service");
+            _listener.Stop();
+        }
+    }
+}
